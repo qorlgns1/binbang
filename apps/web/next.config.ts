61aae2487f827afa44eef1183d61fa024e4b275e
qorlgns1 <rlgns0610@gmail.com>
2026-02-10 15:41:18 +0900

    feat(next.config): add new image host configurations for Kakao and Google
    - Included 'http://img1.kakaocdn.net' and 'https://lh3.googleusercontent.com' to the allowed image host list in the Next.js configuration for enhanced image loading capabilities.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -36,6 +36,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'http',
+        hostname: 'img1.kakaocdn.net',
+      },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
   },
 
